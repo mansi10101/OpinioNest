3659fb3d117b051c8e6ab7da49bb0386b78079c6
Mansi Nagaria <73468933+mansi10101@users.noreply.github.com>
2023-12-20 12:05:32 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 });
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true })
   // .then(() => {
   //   //listen for requests
   //   app.listen(process.env.PORT, () => {
